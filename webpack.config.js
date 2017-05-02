ba209b37618697f571ccbd1e34419db97c1331b1
enotocode <enotocode@yandex.ru>
2017-05-02 13:13:18 +0800

    Add export and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+//const webpack = require('webpack');
+
+module.exports = {
+    entry: "./app/ServiceApp",
+    output: {
+        filename: "./public/build.js",
+        library: "MinesweeperGame"
+    },
+    
+    watch: NODE_ENV == 'development',
+    devtool: NODE_ENV == 'development' ? "eval" : null,
+    
+    //plugins: [
+    //    new webpack.DefinePlugin({
+    //        NODE_ENV: JSON.stringify(NODE_ENV),
+    //        LANG: JSON.stringify('ru')
+    //    })
+    //],
+    
+    //module: {
+    //    rules: [{
+    //        test: /\.js$/,
+    //        use: {
+    //            loader: "babel-loader",
+    //            options: {
+    //                presets: ['es2015'],
+    //                plugins: ['transform-runtime']
+    //            }
+    //        }
+    //    }]
+    //}
+}
