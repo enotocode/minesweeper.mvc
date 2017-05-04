88b1a4693969df651d73d91893d914992cba14d6
enotocode <enotocode@yandex.ru>
2017-05-04 10:51:07 +0800

    Move events to another classes

--- app/GameEvent.js
+++ app/GameEvent.js
@@ -16,19 +16,3 @@ function GameEvent(type, target) {
     this.type = type;
     this.target = target;
 }
-
-/**
- * GameEvent constant describing game's event types
- */
-GameEvent.GAME_OVER = 'GAME_OVER';
-
-GameEvent.UPDATE_CELL_STATUS = 'UPDATE_CELL_STATUS';
-GameEvent.UPDATE_GAME_STATUS = 'UPDATE_GAME_STATUS';
-
-GameEvent.CELL_OPENED = 'CELL_OPENED';
-GameEvent.CELL_MINED = 'CELL_MINED';
-GameEvent.CELL_MARKED = 'CELL_MARKED';
-GameEvent.CELL_UNMARKED = 'CELL_UNMARKED';
-
-GameEvent.SHOW_MINES = 'SHOW_MINES';
-GameEvent.RESTART = 'RESTART';
