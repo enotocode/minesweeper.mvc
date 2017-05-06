32f470dd48d3b3f7dbdeb5ceae17a573ffd40815
enotocode <enotocode@yandex.ru>
2017-05-06 17:17:20 +0800

    class Cell is store cell property only, methods accept cell coordinate as an arguments

--- app/BrowserViewController.js
+++ app/BrowserViewController.js
@@ -42,5 +42,37 @@ function BrowserViewController(view, game) {
         var mines = that._game.getMines();
         that._view.showMines(mines);
     })
+    this.addView();
+}
+
+BrowserViewController.prototype.addView = function() {
+    var view2 = new BrowserView(this._game);
     
+    var that = this;
+    
+    // Restart Game
+    view2.eventDispatcher.subscribe(BrowserView.EVENT_BUTTON_RESTART_CLICK, function() {
+        that._game.restart();
+    })
+
+    
+    // Open cell 
+    view2.eventDispatcher.subscribe(BrowserView.EVENT_CELL_CLICK_LEFT, function(event) {
+        that._game.openCell(event.x, event.y);         
+    })
+
+    
+    // Switching a flag
+    view2.eventDispatcher.subscribe(BrowserView.EVENT_CELL_CLICK_RIGHT, function(event) {
+        that._game.switchFlag(event.x, event.y);
+    })
+
+    
+    // Show mines
+    view2.eventDispatcher.subscribe(BrowserView.EVENT_BUTTON_SHOW_MINES_CLICK, function() {
+        var mines = that._game.getMines();
+        view2.showMines(mines);
+    })
+    
+    view2.render();
 }
