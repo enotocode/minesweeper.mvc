a51a350c10a7cc610c3b4ec4b834f9e20dd6e2fd
enotocode <enotocode@yandex.ru>
2017-05-06 23:10:32 +0800

    Remove target arg

--- app/GameEvent.js
+++ app/GameEvent.js
@@ -9,10 +9,8 @@ module.exports = GameEvent;
  * transferring to observer when event dispatches
  * 
  * @param   {Constant} type - One of the GameEvent constant denoting game's event
- * @param   {Object} target - Target object of event
  * 
  */
 function GameEvent(type, target) {
     this.type = type;
-    this.target = target;
 }
