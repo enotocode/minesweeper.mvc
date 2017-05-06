1182a0a0d7fae34fb9fd855a38107985c54b1db7
enotocode <enotocode@yandex.ru>
2017-05-06 15:40:47 +0800

    Add

---
+++ app/CellEvent.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Export the class
+module.exports = CellEvent;
+
+
+/**
+ * CellEvent store type and cell coordinates
+ * transferring to observer when event dispatches
+ * 
+ * @param   {string} type - One of the GameEvent constant denoting game's event
+ * @param   {number} x - X coordinate of cell
+ * @param   {number} y - Y coordinate of cell
+ * 
+ */
+function GameEvent(type, x, y) {
+    this.type = type;
+    this.x = x;
+    this.y = y;
+}
