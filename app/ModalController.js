14960a72e2c3d8b3f532f61116849e5f2c5f5dcf
enotocode <enotocode@yandex.ru>
2017-05-10 18:29:42 +0800

    Add public method hide() and show()

--- app/ModalController.js
+++ app/ModalController.js
@@ -5,6 +5,8 @@ module.exports = ModalController;
 
 // Dependencies
 var GameEvent = require('./GameEvent');
+var MinesweeperGame = require('./MinesweeperGame');
+var ModalView = require('./ModalView');
 
 /**
  * Controller for modalView
@@ -15,13 +17,37 @@ var GameEvent = require('./GameEvent');
 function ModalController(view, game) {
     
     this._view = view;
-    this._game = game;
-    
+    this._game = game;    
+  
     var that = this;
     
     // Restart Game
-    this._view.eventDispatcher.subscribe(GameEvent.RESTART, function() {
+    this._view.eventDispatcher.subscribe(ModalView.BUTTON_RESTART_CLICK, function() {
         that._game.restart();
-    }) 
+    })       
+}
+/**
+ * Run windget, use to place widget in DOM
+ * 
+ * @returns {DOMObject} - DOM Object with modal window
+ */
+ModalController.prototype.run = function() {    
     
+    return this._view.run();
+}
+
+/**
+ * Hide modal window
+ */
+ModalController.prototype.hide = function () {
+    
+    this._view.hide();
+}
+
+/**
+ * Show modal window
+ */
+ModalController.prototype.show = function () {
+    
+    this._view.show();
 }
