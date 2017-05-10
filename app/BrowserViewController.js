5db4d40191b61dd8035110245ec9252848bc0536
enotocode <enotocode@yandex.ru>
2017-05-10 18:18:34 +0800

    Take control on modal window

--- app/BrowserViewController.js
+++ app/BrowserViewController.js
@@ -6,16 +6,21 @@ module.exports = BrowserViewController;
 // Dependencies
 var GameEvent = require('./GameEvent');
 var BrowserView = require('./BrowserView');
+var ModalController = require('./ModalController');
+var ModalView = require('./ModalView');
+var MinesweeperGame = require('./MinesweeperGame');
 
 /**
  * Controller for browserView
  * 
- * @param   {BrowserView} view
- * @param   {MinesweeperGame} game
+ * @param   {BrowserView} _view 
+ * @param   {MinesweeperGame} _game
+ * @param   {ModalController} _modal 
  */
 function BrowserViewController(view, game) {
     this._view = view;
     this._game = game;
+    this._modal = null;
     
     var that = this;
     
@@ -42,5 +47,31 @@ function BrowserViewController(view, game) {
         var mines = that._game.getMines();
         view.showMines(mines);
     })
+    
+    this.createModal();
+}
+
+/**
+ * Create modal window widget
+ */
+BrowserViewController.prototype.createModal = function() {
+    
+    // Create modal view and controller
+    this._modal = new ModalController(new ModalView(), this._game);
+    // Inject modal window DOM object in View
+    this._view.insertElement(this._modal.run());
+    
+    var that = this;
+    
+    // Order modal window show and hide
+    this._game.eventDispatcher.subscribe(MinesweeperGame.EVENT_UPDATE_GAME_STATUS, function (event) {
+        if (event.status === MinesweeperGame.STATUS_LOSE ||
+            event.status === MinesweeperGame.STATUS_WIN) {
+            that._modal.show();
+        };
+        if (event.status === MinesweeperGame.STATUS_PLAYING) {
+            that._modal.hide();
+        };        
+    });
 }
 
