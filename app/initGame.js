dfa5a305551a2c06231a1349b0d1103668755483
enotocode <enotocode@yandex.ru>
2017-05-10 18:44:20 +0800

    New formatting and replace initializing modal window in BrowserWindowController

--- app/initGame.js
+++ app/initGame.js
@@ -1,30 +1,24 @@
 'use strict';
 
-// Create game Model
+// Dependencies
 var MinesweeperGame = require('./MinesweeperGame');
+var BrowserView = require('./BrowserView');
+var BrowserViewController = require('./BrowserViewController');
+var ConsoleController = require('./ConsoleController');
+
+// Create game Model
 var mswprGame = new MinesweeperGame();
 
 // Create browser View
-var BrowserView = require('./BrowserView');
 var browserView = new BrowserView(mswprGame);
 
-// Create modal View
-var ModalView = require('./ModalView');
-var modalView = new ModalView();
-
 // Create browser Controller
-var BrowserViewController = require('./BrowserViewController');
 var browserViewController = new BrowserViewController(browserView, mswprGame);
 
 // Render field in browser
 browserView.render();
 
-var ModalController = require('./ModalController');
-var modalViewController = new ModalController(modalView, mswprGame);
-modalView.attach(mswprGame);
-
 // Create console Controller
-var ConsoleController = require('./ConsoleController');
 var game = new ConsoleController();
 game.attach(mswprGame);
 
