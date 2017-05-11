432dc16eea11ca8a6f5892dd0d6262428ce82824
enotocode <enotocode@yandex.ru>
2017-05-11 13:58:31 +0800

    Attach and subscribe model events in constructor

--- app/ConsoleController.js
+++ app/ConsoleController.js
@@ -9,30 +9,24 @@ var MinesweeperGame = require('./MinesweeperGame');
 
 /**
  * Console controller class
+ * for game control with text commands (ex: in browser console)
  * 
  * @property {MinesweeperGame} _model - The game
  * @property {String} _field - Game field
+ * @property {Object} _charSet  - Chars presenting cell status in text mode
  */
-function ConsoleController() {
-    
-    this._model = null;
-    this._field = '';
-    this.charSet = {};
-    
-    this.charSet[MinesweeperGame.EVENT_CELL_OPENED] = '.';
-    //this.charSet[MinesweeperGame.E] = '*';
-    this.charSet[MinesweeperGame.EVENT_CELL_MARKED] = 'F';
-    this.charSet[MinesweeperGame.EVENT_CELL_UNMARKED] = '+';
-    
-};
-
-/**
- * @param   {MinesweeperGame} model - The game
- */
-ConsoleController.prototype.attach = function(model) {
+function ConsoleController(model) {
     
     this._model = model;
+    this._field = '';
+    this._charSet = {};
     
+    this._charSet[MinesweeperGame.EVENT_CELL_OPENED] = '.';
+    //this._charSet[MinesweeperGame.E] = '*';
+    this._charSet[MinesweeperGame.EVENT_CELL_MARKED] = 'F';
+    this._charSet[MinesweeperGame.EVENT_CELL_UNMARKED] = '+';
+    
+    // Subscribing
     var that = this;
     
     this._model.eventDispatcher.subscribe( MinesweeperGame.EVENT_CELL_OPENED, function (event) {
@@ -50,15 +44,18 @@ ConsoleController.prototype.attach = function(model) {
     this._model.eventDispatcher.subscribe( MinesweeperGame.EVENT_GAME_RESTART, function () {
         that.redraw();
     });    
-   
-}
+    
+};
+
 
 /**
- * @argument {string} coordinates - Format a3, 4b etc
+ * Open cell
+ * 
+ * @argument {string} - Cell coordinates in chess style: a3, 4b etc
  */
 ConsoleController.prototype.open = function(coordinates) {
     
-    var cell = this.createCellFromString(coordinates);
+    var cell = this.transformChessCoordinates(coordinates);
     
     if (coordinates && this._model.openCell(cell.x, cell.y)) {
         
@@ -72,11 +69,13 @@ ConsoleController.prototype.open = function(coordinates) {
 }
 
 /**
- * @argument {string} coordinates - Format a3, 4b etc
+ * Set flag
+ * 
+ * @argument {string} - Cell coordinates in chess style: a3, 4b etc
  */
 ConsoleController.prototype.setFlag = function(coordinates) {
     
-    var cell = this.createCellFromString(coordinates);
+    var cell = this.transformChessCoordinates(coordinates);
     
     if (coordinates) {
         
@@ -86,11 +85,13 @@ ConsoleController.prototype.setFlag = function(coordinates) {
 }
 
 /**
- * @argument {string} coordinates - Format a3, 4b etc
+ * Remove flag
+ * 
+ * @argument {string} - Cell coordinates in chess style: a3, 4b etc
  */
 ConsoleController.prototype.removeFlag = function(coordinates) {    
     
-    var cell = this.createCellFromString(coordinates);
+    var cell = this.transformChessCoordinates(coordinates);
     
     if (coordinates) {
         
@@ -100,7 +101,7 @@ ConsoleController.prototype.removeFlag = function(coordinates) {
 }
 
 /**
- *
+ * Loose
  */
 ConsoleController.prototype.resign = function() {
     
@@ -109,7 +110,7 @@ ConsoleController.prototype.resign = function() {
 }
 
 /**
- *
+ * Restart game
  */
 ConsoleController.prototype.restart = function() {
     
@@ -118,12 +119,12 @@ ConsoleController.prototype.restart = function() {
 }
 
 /**
- * Create Cell object from chess coordinates
+ * Transform chess style coordinates in {x, y} 
  * 
  * @argument {string} string - Chess style coordinates a3, b1 etc
- * @returns {(Cell|null)} cell - Cell object or null in case of wrong coordinates
+ * @returns {{x: number, y: number}} - Cell coordinates
  */
-ConsoleController.prototype.createCellFromString = function(string) {
+ConsoleController.prototype.transformChessCoordinates = function(string) {
     
     var letter = ['A' ,'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         
@@ -157,7 +158,7 @@ ConsoleController.prototype.updateCellStatus = function(status, x, y){
     }
     
     var charNumber = x * 2 + 20 * y;    
-    var newChar = this.charSet[status];    
+    var newChar = this._charSet[status];    
     var field = this._field;
     var minesQantity = this._model.getMinesQuantity(x, y);
 
@@ -206,7 +207,9 @@ ConsoleController.prototype.createField = function() {
     // Save actual field in this._field
     this._field = field;
 }
-
+/**
+ * Redraw field
+ */
 ConsoleController.prototype.redraw = function() {
     this.createField();
     this.show();
